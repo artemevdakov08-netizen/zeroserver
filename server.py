56a820d3c3abd2a07abb818ec70118ef7a7b3f05
artemevdakov08-netizen <artemevdakov08@gmail.com>
2025-11-01 21:29:23 +0600

    Update server.py

--- server.py
+++ server.py
@@ -56,8 +56,10 @@ def promo():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(host="0.0.0.0", port=port
+debug=True)
     
+
 
 
 
